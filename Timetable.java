3808fccf2a93e31c5a02795d87637a9abdd13cd2
Joshua_cse <joshuarubert.r2024cse@sece.ac.in>
2025-09-15 07:42:19 +0530

    Class teacher partially completed

--- Timetable.java
+++ Timetable.java
@@ -1,5 +1,25 @@
 import java.util.*;
-
+class Teacher{
+    String name;
+    int teacher_id;
+    String deapartment;
+    Teacher(String name, int id,String Deapartment){
+        this.name = name;
+        this.teacher_id = id;
+        this.deapartment = Deapartment;
+    }void displayTeacherDetails(){
+        System.out.println("Name: "+name);
+        System.out.println("ID: "+teacher_id);
+        System.out.println("Deapartment: "+deapartment);
+    }
+}public class section{
+    String name;
+    String id;
+    section(String name, String id){
+        this.name = name;
+        this.id = id;
+    }
+}
 class schedule{
     String subject;
     String time;
