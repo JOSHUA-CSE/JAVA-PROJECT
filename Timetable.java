96f7c0594918018f5673dfdb10c5bec3676d6e1a
Joshua_cse <joshuarubert.r2024cse@sece.ac.in>
2025-09-15 07:37:49 +0530

    class schedule declared

--- Timetable.java
+++ Timetable.java
@@ -1,5 +1,16 @@
 import java.util.*;
 
+class schedule{
+    String subject;
+    String time;
+    String day;
+    Teacher teacher;
+    Section section;
+    schedule(String subject, String time){
+        this.subject = subject;
+        this.time = time;
+    }
+}
 public class Timetable {   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
